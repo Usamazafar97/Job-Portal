d6803873c8a0581b9ac2602c809ec3971ecb96ec
Usama Zafar <osamazafar97gmail.com>
2024-05-27 01:59:04 +0200

    service and repository layers added successfully

--- src/main/java/com/example/JobApp/model/JobPost.java
+++ src/main/java/com/example/JobApp/model/JobPost.java
@@ -3,13 +3,14 @@ package com.example.JobApp.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-
+@Component
 public class JobPost {
     private int postId;
     private String PostProfile;
