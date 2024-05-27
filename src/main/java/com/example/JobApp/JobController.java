ffb0252b56012be79b96432ebb2ec34b0d3e487f
Usama Zafar <osamazafar97gmail.com>
2024-05-27 02:27:26 +0200

    view all jobs added successfully

--- src/main/java/com/example/JobApp/JobController.java
+++ src/main/java/com/example/JobApp/JobController.java
@@ -4,9 +4,9 @@ import com.example.JobApp.model.JobPost;
 import com.example.JobApp.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class JobController {
@@ -28,5 +28,11 @@ public class JobController {
     public String handleForm(JobPost jobPost){
         jobService.addJob(jobPost);
         return "success";
+    }
+
+    @GetMapping("viewalljobs")
+    public String viewAllJobs(Model model){
+        model.addAttribute("jobPosts", jobService.showJobs());
+        return "viewalljobs";
     }
 }
